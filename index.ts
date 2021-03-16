64e0ccad4c2e33b7c1a92d3c0c6ae2d444adca54
Anwesh43 <anweshthecool0@gmail.com>
2021-03-16 23:58:46 +0530

    adding InputContainer class

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 class CustomElement {
 
-    public style : String 
+    public style : string 
     initStyle() {
         return {
 
@@ -9,7 +9,7 @@ class CustomElement {
 
     parseStyle() {
         const styleObj = this.initStyle()
-        let styleStr = ``
+        let styleStr : string = ``
         Object.keys(styleObj).forEach((styleKey) => {
             const styleValue = styleObj[styleKey]
             styleStr = `${styleStr}${styleKey}:${styleValue};`
@@ -18,7 +18,7 @@ class CustomElement {
     }
 
 
-    render() : String {
+    render() : string {
         this.parseStyle()
         return ""
     }
@@ -43,3 +43,62 @@ class Renderer {
 }
 
 const renderer : Renderer = new Renderer()
+
+
+class InputBox extends CustomElement {
+
+    value : string
+    initStyle() {
+        return {
+            width: "50%"
+        }
+    }
+
+    render() : string {
+        super.render()
+        return `<input id = "todoInput" type = "text" placeholder = "Enter an item" style = "${this.style}">`
+    }
+}
+
+class ButtonElement extends CustomElement {
+
+    initStyle() {
+        return {
+            width : "30%"
+        }  
+    }
+
+    render() : string {
+        super.render()
+        console.log("BTN_sTYLE", this.style)
+        return `<button id = "btn1" style = "${this.style}">Add Todo</button>`
+    }
+}
+
+class InputContainer extends CustomElement {
+
+    children : Array<CustomElement> = []
+
+    initStyle() {
+        return {
+            width: "50%",
+            display: "flex",
+            "justify-content": "space-between"
+        }
+    }
+    add(element : CustomElement) {
+        this.children.push(element)
+    }
+
+    render() : string {
+        super.render()
+        return `<div style = "${this.style}">${this.children.map((element) => element.render()).join("\n")}</div>`
+    }
+}
+
+const inputContainer = new InputContainer()
+const buttonElement = new ButtonElement()
+const inputBox : InputBox = new InputBox()
+inputContainer.add(inputBox)
+inputContainer.add(buttonElement)
+renderer.add(inputContainer)
